0fc0e9ad2be8c0cb4cdc4cb767765e614a5176ec
joseluistineo90 <joseluistineo90@gmail.com>
2017-05-21 18:16:56 -0400

    inicio de web el sol

---
+++ index.php
@@ -0,0 +1,286 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Rompe tickets|El Sol|Legal services</title>
+
+		<meta name="description" content="Problemas legales en las VEGAS NEVADA, con taxes, drogas, divorcios rápidos, maltrato familiar , casos de crímenes felonías y tickets de tráfico" />
+		<meta name="keywords" content="drogas, felonías, casos criminales, problemas legales, taxes, trafic taxes" />
+		<meta name="author" content="José Tineo" />
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="shortcut icon" href="imageson.ico" type="image/x-icon" />
+<link rel="apple-touch-icon" sizes="57x57" href="images/apple-touch-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="images/apple-touch-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="images/apple-touch-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="images/apple-touch-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="images/apple-touch-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="images/apple-touch-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="images/apple-touch-icon-180x180.png">
+<link rel="icon" type="image/png" href="images/favicon-16x16.png" sizes="16x16">
+<link rel="icon" type="image/png" href="images/favicon-32x32.png" sizes="32x32">
+<link rel="icon" type="image/png" href="images/favicon-96x96.png" sizes="96x96">
+<link rel="icon" type="image/png" href="images/android-chrome-192x192.png" sizes="192x192">
+<meta name="msapplication-square70x70logo" content="images/smalltile.png" />
+<meta name="msapplication-square150x150logo" content="images/mediumtile.png" />
+<meta name="msapplication-wide310x150logo" content="images/widetile.png" />
+<meta name="msapplication-square310x310logo" content="images/largetile.png" />
+
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+		<link rel="stylesheet" href="./assets/css/style.css" />
+		<script src="./assets/js/modernizr.custom.63321.js"></script>
+	</head>
+	<body>
+
+
+
+		<!-- Header -->
+			<header id="header">
+				<h1>Rompe Tickets</h1>
+				<!--botón-->
+
+				<form align=center action="" autocomplete="off">
+    			<a href="language.html"><input type="button" value="English" id="toggle" /></a>
+    			<label for="toggle"></label>
+    			</form>
+
+				<!--Fin del toggle button-->
+				<nav>
+					<ul>
+						<li><a href="#intro">Intro</a></li>
+						<li><a href="#one">Ventajas</a></li>
+						<li><a href="#two">Servicios</a></li>
+						<li><a href="#work">Dónde ubicarnos</a></li>
+						<li><a href="#contact">Contacto</a></li>
+					</ul>
+				</nav>
+			</header>
+
+		<!-- Intro -->
+			<section id="intro" class="main style1 dark fullscreen">
+				<div class="content">
+					<header>
+						<h2 class="degree">EL SOL Servicios Legales</h2>
+					</header>
+					<p class="intro">¿Problemas con la ley? <strong>Levantamos tus órdenes de arresto el mismo día a partir de 25 $.</strong><br> Rompe Tickets & Traffic Tickets.
+					TAXES todo el año, renueva tu ITIN .</p>
+					<footer>
+					<img src="images/thumbs/sol_luna2LogoSpa.gif" title="The Anonymous Red" alt="" />
+						<!--<a href="#one" class="button style2 down">More</a>-->
+					</footer>
+				</div>
+			</section>
+
+		<!-- One -->
+			<section id="one" class="main style2 right dark fullscreen">
+				<div class="content box style2">
+					<header>
+						<h2>Ventajas</h2>
+					</header>
+					<p>Por hacer tus taxes con nosotros, te arreglamos un ticket de tráfico gratis. Extensión de 3 meses, plan de pago, no punto, no cortes, no escuela. </p>
+				</div>
+				<a href="#two" class="button style2 down anchored">Next</a>
+			</section>
+
+		<!-- Two -->
+			<section id="two" class="main style2 left dark fullscreen">
+				<div class="content box style2">
+					<header>
+						<h2>Servicios</h2>
+					</header>
+					<p>Casos criminales , robos, felonías. Dui's, problemas legales con drogas, divorcios rápidos.</p>
+				</div>
+				<a href="#work" class="button style2 down anchored">Next</a>
+			</section>
+
+		<!-- Work -->
+			<section id="work" class="main style3 primary">
+				<div class="content">
+					<header>
+						<h2>¿Dónde ubicarnos?</h2>
+						<p>1813 S. EASTERN AVE.<h3>** LAS VEGAS **</h3> Nevada 89104.USA
+						phone: 702-743-4327.</p>
+					</header>
+	<div class="loader"></div>			
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3926.2631700433076!2d-66.85928738571656!3d10.240346171660711!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8c2aeeffea700273%3A0xc6eb0666c486e44c!2sBiblioteca+p%C3%BAblica+Rafael+Vicente+Egui%2C+Calle+Dr.Jose+G+Hernandez%2C+Charallave%2C+Miranda!5e0!3m2!1ses!2sve!4v1479623248735" width="800" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+    <div class="loader"></div>
+					<section class="social">				
+					<p>Contáctenos directamente con su celular o smartphone  pulsando el siguiente botón a El SOL legal services.</p>
+    <p><a href="tel:7027434327"  title="ring"  class="phone"><b>Tlf : 7027434327</b></a></p>
+    </section>
+
+					<!-- Gallery  -->
+						<div class="gallery">
+							<article class="from-left">
+								<a href="images/fulls/01.jpg" class="image fit"><img src="images/thumbs/1.jpg" title="The Anonymous Red" alt="" /></a>
+							</article>
+							<article class="from-right">
+								<a href="images/fulls/02.jpg" class="image fit"><img src="images/thumbs/2.jpg" title="Airchitecture II" alt="" /></a>
+							</article>
+							<article class="from-left">
+								<a href="images/fulls/03.jpg" class="image fit"><img src="images/thumbs/3.jpg" title="Air Lounge" alt="" /></a>
+							</article>
+							<article class="from-right">
+								<a href="images/fulls/4.jpg" class="image fit"><img src="images/thumbs/4.jpg" title="Carry on" alt="" /></a>
+							</article>
+							<article class="from-left">
+								<a href="images/fulls/05.jpg" class="image fit"><img src="images/thumbs/5.jpg" title="The sparkling shell" alt="" /></a>
+							</article>
+							<article class="from-right">
+								<a href="images/fulls/06.jpg" class="image fit"><img src="images/thumbs/6.jpg" title="Bent IX" alt="" /></a>
+							</article>
+						</div>
+
+				</div>
+			</section>
+			<!--Slider-->
+	<div class="container">	
+			
+			<header class="clearfix">
+				<h2>Somos <span>accesibles, responsables y diligentes</span></h2>
+			</header>
+			<div class="main">
+				<div id="mi-slider" class="mi-slider">
+					<ul>
+						<li><a href="#"><img src="images/1.jpg" alt="img01"><h4>Rápidos</h4></a></li>
+						<li><a href="#"><img src="images/2.jpg" alt="img02"><h4>Oxfords</h4></a></li>
+						<li><a href="#"><img src="images/3.jpg" alt="img03"><h4>Loafers</h4></a></li>
+						
+					</ul>
+					<ul>
+						<li><a href="#"><img src="images/5.jpg" alt="img05"><h4>Belts</h4></a></li>
+						
+						<li><a href="#"><img src="images/7.jpg" alt="img07"><h4>Sunglasses</h4></a></li>
+						
+					</ul>
+					<ul>
+						<li><a href="#"><img src="images/9.jpg" alt="img09"><h4>Casual</h4></a></li>
+						<li><a href="#"><img src="images/10.jpg" alt="img10"><h4>Luxury</h4></a></li>
+						<li><a href="#"><img src="images/11.jpg" alt="img11"><h4>Sport</h4></a></li>
+					</ul>
+					<ul>
+						<li><a href="#"><img src="images/12.jpg" alt="img12"><h4>Carry-Ons</h4></a></li>
+						<li><a href="#"><img src="images/13.jpg" alt="img13"><h4>Duffel Bags</h4></a></li>
+						<li><a href="#"><img src="images/14.jpg" alt="img14"><h4>Laptop Bags</h4></a></li>
+						
+					</ul>
+					<nav>
+						<a href="#">Divorcios</a>
+						<a href="#">Drogas</a>
+						<a href="#">Felonías</a>
+						<a href="#">Taxes</a>
+					</nav>
+				</div>
+			</div>
+		</div><!-- /container -->
+	<!--Fin Slider-->
+		<!-- Contact -->
+	
+			<section id="contact" class="main style3 secondary">
+				<div class="content">
+					<header>
+						<h2>Escríbanos.</h2>
+						<p>Déjenos su mensaje acá o en nuestras redes sociales y le estaremos contactando.</p>
+					</header>
+					<!--<div class="box">
+						<form method="post" action="#">
+							<div class="field half first"><input type="text" name="name" placeholder="Name" /></div>
+							<div class="field half"><input type="email" name="email" placeholder="Email" /></div>
+							<div class="field"><textarea name="message" placeholder="Message" rows="6"></textarea></div>
+							<ul class="actions">
+								<li><input type="submit" value="Enviar Mensaje" /></li>
+							</ul>
+						</form>
+					</div>-->
+					<?php
+$errores = '';
+$enviado = '';
+
+if (isset($_POST['submit'])) {
+	$nombre = $_POST['nombre'];
+	$correo = $_POST['correo'];
+	$mensaje = $_POST['mensaje'];
+
+	if (!empty($nombre)) {
+		$nombre = trim($nombre);
+		$nombre = filter_var($nombre, FILTER_SANITIZE_STRING);
+	} else {
+		$errores .= 'Por favor ingrese nombre <br />';
+		}
+
+		if (!empty($correo)){
+			$correo = filter_var($correo, FILTER_SANITIZE_EMAIL);
+			if (!filter_var($correo, FILTER_VALIDATE_EMAIL)){
+				$errores .= 'Favor ingrese un correo valido <br />';
+			}
+		
+	} else { 
+		$errores .= 'Por favor ingrese un correo <br >';
+}
+ if(!empty($mensaje)){
+ 	$mensaje = htmlspecialchars($mensaje);
+ 	$mensaje = trim($mensaje);
+ 	$mensaje = stripslashes($mensaje);
+ } else {
+ 	$errores .= 'Por favor ingrese mensaje<br />';
+ }
+ 	if (!$errores){
+ 		$enviar_a = 'joseluistineo90@gmail.com';
+ 		$asunto = 'Correo enviado desde mi página.com';
+ 		$mensaje_preparado = "De : $nombre \n";
+ 		$mensaje_preparado .= "Correo : $correo \n";
+ 		$mensaje_preparado .= "Mensaje : .$mensaje";
+ 		mail($enviar_a, $asunto, $mensaje_preparado);
+ 		$enviado = 'true';
+ 	}
+}
+require 'mail.php';
+?>
+				</div>
+			</section>
+
+		<!-- Footer -->
+			<footer id="footer">
+
+				<!-- Icons -->
+					<ul class="actions">
+						<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+						<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+						<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+						<li><a href="#" class="icon fa-linkedin"><span class="label">LinkedIn</span></a></li>
+						
+					</ul>
+
+				<!-- Menu -->
+					<ul class="menu">
+						<li>&copy; Licencia MIT</li><li>Design: <a href="https://josetineocv.arredemo.org" target="_blank">CaracasWebDesign</a></li>
+					</ul>
+
+			</footer>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.poptrox.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+			<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+		<script src="assets/js/jquery.catslider.js"></script>
+		<script>
+			$(function() {
+
+				$( '#mi-slider' ).catslider();
+
+			});
+		</script>
+		<!--script del toogle button language-->
+		
+	</body>
+</html>
